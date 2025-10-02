f4839417b213cc18a5c1d5c572f3c5e12ce5213e
Leon Choi <leon.choi.dev@gmail.com>
2025-10-02 13:01:07 +0900

    Add Java solution for 1. Two Sum

---
+++ arrays/1_two_sum.java
@@ -0,0 +1,16 @@
+// Approach 1: Brute Force
+// Time complexity: O(n²)
+// Space complexity: O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return int[]{};
+    }
+}
