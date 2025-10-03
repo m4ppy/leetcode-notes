759e30a1cbcb50035e9e34e0a40f7f2ca89ef979
Leon Choi <leon.choi.dev@gmail.com>
2025-10-03 23:35:46 +0900

    Add Java solution for 217. Contains Duplicate (Brute Force)

--- arrays/217_contains_duplicate.java
+++ arrays/217_contains_duplicate.java
@@ -1,4 +1,21 @@
 // Approach 2: Hash Set
+// Time complexity: O(n²)
+// Space complexity: O(1)
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// Approach 2: Hash Set
 // Time complexity: O(n) traverse the list once.
 // Space complexity: O(n) for hash set.
 
