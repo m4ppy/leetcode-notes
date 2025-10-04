e2b7bac722f1024d70fe15a6d4be15ca34019ae8
Leon Choi <leon.choi.dev@gmail.com>
2025-10-04 23:07:47 +0900

    Add Python solution for 15. 3Sum

---
+++ strings/15_3Sum.py
@@ -0,0 +1,15 @@
+# Approach 1: Brute Force
+# Time complexity: O(n3)
+# Space complexity: O(m) m is the number of triplets.
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        tmp = [nums[i], nums[j], nums[k]]
+                        res.add(tuple(tmp))
+        return [list(i) for i in res]
