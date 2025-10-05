1b3abf659525941218c0e7397d15ac6b95a85c9b
Leon Choi <leon.choi.dev@gmail.com>
2025-10-05 23:53:17 +0900

    Add Python solution for 15. 3Sum (Two Pointers)

--- strings/15_3Sum.py
+++ strings/15_3Sum.py
@@ -42,3 +42,32 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 count[nums[j]] += 1
         return res
+
+# Approach 3: Two Pointers
+# Time complexity: O(n²)
+# Space complexity: O(n) for hash map.
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i, n in enumerate(nums):
+            if n>0:
+                break
+            if i>0 and n == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
+            while left < right:
+                Sum1 = n + nums[left] + nums[right]
+                if Sum1 > 0:
+                    right -= 1
+                elif Sum1 < 0:
+                    left += 1
+                else:
+                    result.append([n, nums[left], nums[right]]) 
+                    right -= 1
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1 
+        return result
