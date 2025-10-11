58903cb17b7f5298610a7d86f9b503b4f77bb198
Leon Choi <leon.choi.dev@gmail.com>
2025-10-11 21:06:08 +0900

    Update comment for 49. Group Anagrams

--- strings/49_group_anagrams.py
+++ strings/49_group_anagrams.py
@@ -1,5 +1,5 @@
 # Approach 1: Sorting
-# Time complexity: O(m ∗ n log n) m is number of strings and n is length of the logest string. sorting that string and traverse strs once.
+# Time complexity: O(m ∗ n log n) m is number of strings and n is length of the logest string. sort string while traversing given list.
 # Space complexity: O(m * n) for hash map.
 
 class Solution:
@@ -15,3 +15,24 @@ class Solution:
                 res[key] = [s]
 
         return list(res.values())
+
+# Approach 2: Hash Table
+# Time complexity: O(m ∗ n) m is number of strings and n is length of the logest string. count each string while traversing given list.
+# Space complexity: O(m * n) for hash map that stores m keys that value is n.
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            key = tuple(count)
+            if key in hashMap:
+                hashMap[key].append(s)
+            else:
+                hashMap[key] = [s]
+
+        return list(hashMap.values())
