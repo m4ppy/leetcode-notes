82ba7aa44223d74310e175c273da819ed9fbb900
Leon Choi <leon.choi.dev@gmail.com>
2025-10-12 16:38:33 +0900

    Add Java solution for 347. Top K Frequent Elements

---
+++ sorting/347_top_k_frequent_elements.java
@@ -0,0 +1,87 @@
+// Approach 1: Sorting
+// Time complexity: O(n log n) initialize dictionary for count and list for sort + sort the list
+// Space complexity: O(n) count and list for sort.
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        }
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
+        }
+        return res;
+    }
+}
+
+// Approach 2-1: Min-Heap
+// Time complexity: O(n log k) build a frequency map in O(n) + maintain a min-heap of size k to track top frequencies in O(m log k) + extract result in O(k)
+// Space complexity: O(n + k) count + heap
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            heap.offer(new int[]{entry.getValue(), entry.getKey()});
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
+        }
+
+        return res;
+    }
+}
+
+// Approach 3: Bucket Sort
+// Time complexity: O(n)
+// Space complexity: O(n) for count(n) + freq(n+1)
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
