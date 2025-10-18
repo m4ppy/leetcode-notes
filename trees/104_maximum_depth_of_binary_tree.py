9b2e5620470ff99e74d6dd870f2814bdbd2dc678
Leon Choi <leon.choi.dev@gmail.com>
2025-10-18 16:23:32 +0900

    Add Python solution for 104. Maximum Depth of Binary Tree

---
+++ trees/104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,56 @@
+# Approach 1: Breath First Search
+# Time complexity: O(n)
+# Space complexity: O(h) worst case: O(n) best case: O(log n)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        depth = 0
+
+        if not root:
+            return depth
+
+        queue = deque([root])
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+
+        return depth
+
+# Approach 2: Depth First Search
+# Time complexity: O(n)
+# Space complexity: O(n) for call stack.
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# Approach 3: Iterative DFS
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        depth = 0
+
+        if not root:
+            return depth
+
+        stack = [(root, 1)]
+        while stack:
+            for _ in range(len(stack)):
+                node, cur = stack.pop()
+                depth = max(cur, depth)
+                if node.left:
+                    stack.append((node.left, 1 + cur))
+                if node.right:
+                    stack.append((node.right, 1 + cur))
+
+        return depth
